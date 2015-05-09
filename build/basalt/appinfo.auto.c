091e69f13b93c8bbb1abf1b69f8b97c8d8af20df
James <james.j.downs@gmail.com>
2015-05-09 00:59:19 -0400

    Clean code

--- build/basalt/appinfo.auto.c
+++ build/basalt/appinfo.auto.c
@@ -10,7 +10,7 @@ const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header")))
   .offset = 0xb6b6b6b6,
   .crc = 0xb6b6b6b6,
   .name = "PokeBattle",
-  .company = "james.j.downs@gmail.com",
+  .company = "James Downs",
   .icon_resource_id = DEFAULT_MENU_ICON,
   .sym_table_addr = 0xA7A7A7A7,
   .flags = PROCESS_INFO_WATCH_FACE,
